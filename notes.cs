6955173316309a55c1183a776b4022b00cdc8651
ms-401 <m_sarfraz@hotmail.com>
2026-04-07 22:34:16 +0300

    ..

--- notes.cs
+++ notes.cs
@@ -35,3 +35,5 @@ docker run --network none  --user 1000:1000   -it --rm   --env-file .env -v ~/.o
 docker build -t agent-oh:0.1.15 .
 
 docker pull docker pull quay.io/pragyan-redhat/agents
+
+{"language":"en","agent":"CodeActAgent","max_iterations":null,"security_analyzer":"llm","confirmation_mode":false,"llm_model":"ollama/qwen3-coder-next:latest","llm_api_key":"123","llm_base_url":"http://192.168.3.64:11434/v1","user_version":null,"remote_runtime_resource_factor":1,"secrets_store":{"provider_tokens":{}},"enable_default_condenser":true,"enable_sound_notifications":false,"enable_proactive_conversation_starters":false,"enable_solvability_analysis":false,"user_consents_to_analytics":false,"sandbox_base_container_image":null,"sandbox_runtime_container_image":null,"mcp_config":{"sse_servers":[],"stdio_servers":[],"shttp_servers":[]},"search_api_key":"123","sandbox_api_key":null,"max_budget_per_task":null,"condenser_max_size":240,"email":"","email_verified":true,"git_user_name":"openhands","git_user_email":"openhands@all-hands.dev","v1_enabled":true}
